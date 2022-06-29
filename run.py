fb015a4acc10e63abc9238d3a759dc652632f353
unkhan87 <ukhan87@icloud.com>
2022-06-29 00:17:55 +0000

    Add basic structure for the welcome section

--- run.py
+++ run.py
@@ -9,8 +9,9 @@ def welcome_msg():
     """
     Welcome message for the user.
     """
+    user_name = input('Please enter your name: \n')
     print(
-        'Welcome to Hangman!\n'
+        f'Hi {user_name}, Welcome to Hangman!\n'
     )
 
     view_instructions = input(
@@ -31,12 +32,23 @@ def welcome_msg():
     
 def instructions():
     """
-    Show instructions to the user
+    Show instructions to the user and confirm if the user is ready to play
     """
     print(
         'Instructions:\n\n To play hangman you will need to guess your word letter by letter.\n\n1. Start guessing'
         'the letter and hit enter.\n2. If your letter is correct it one of the blank space will get populated.\n3. If'
         'you gussed an incorrect letter you will get a strike and a section of a hangman will appear.\n4. You have got'
-        '6 tries to guess the word.'
+        '6 tries to guess the word.\n'
     )
+
+    #Confirm if the user is ready to play the game
+    print("Are you ready to play Hangman?")
+    play = input('Please type 1 for yes and 2 for no:\n')
+    #this is to check if user input invalid number
+    while play != "1" and play != "2":
+        play = input(
+            '/nInvalid Input, if you are ready to play '
+            'enter 1 if not enter 2:/n'
+        )
+
 welcome_msg()

037392e83e63793b715b2b0db2a496e7950d50ee
unkhan87 <ukhan87@icloud.com>
2022-07-02 02:00:14 +0000

    Whiteline spacing error and continuation line under-indented now resolved

--- run.py
+++ run.py
@@ -15,32 +15,33 @@ def welcome_msg():
     print(
         f'Hi {user_name}, Welcome to Hangman!\n'
     )
-
-    view_instructions = input(
-        'Please type 1 to see the instructions, ' 'or 2 to start the the game!:\n' 
-    )
-    
+    view_instructions = input('Please type 1 to see the instructions'
+                              ' or 2 to start the the game!:\n')
     # This is to make sure the input is valid
     while view_instructions != "1" and view_instructions != "2":
         view_instructions = input(
-            'Invalid input, Please type 1 to see the instructions or 2 to start the game'
-        )
-    
+            'Invalid input, Please type 1 to see the'
+            'instructions or 2 to start the game'
+            )
     # Take user to choice they made on the first step
     if view_instructions == "1":
         instructions()
     else:
         get_word()
-    
+
+
 def instructions():
     """
     Show instructions to the user and confirm if the user is ready to play
     """
     print(
-        'Instructions:\n\n To play hangman you will need to guess your word letter by letter.\n\n1. Start guessing '
-        'the letter and hit enter.\n2. If your letter is correct one of the blank space will get populated.\n3. If '
-        'you gussed an incorrect letter you will get a strike and a section of a hangman will appear.\n4. You have got '
-        '6 tries to guess the word.\n'
+        'Instructions:\n\n To play hangman you will need to guess your'
+        'word letter by letter.\n\n1. Start guessing'
+        'the letter and hit enter.\n2. If your letter is correct one of'
+        'the blank space will get populated.\n3.'
+        ' If you gussed an incorrect letter you will get a strike and a section'
+        'of a hangman will appear.\n4. You have got'
+        ' 6 tries to guess the word.\n'
     )
 
     # Confirm if the user is ready to play the game
@@ -52,16 +53,17 @@ def instructions():
             '\nInvalid Input, if you are ready to play '
             'enter 1 if not enter 2:\n'
         )
-
     get_word()
+
 
 def get_word():
     word = random.choice(words_list)
     play(word.upper())
 
+
 def play(word):
     """
-    get random words from the words list, and ask the user to guess the words 
+    get random words from the words list, and ask the user to guess the words
     """
     # Initialize variables
     # Blanks for each letter in the word
@@ -69,7 +71,7 @@ def play(word):
     word_guessed = False
     tries = 6
     # used letters tracker
-    used_letters=[]
+    used_letters = []
     # Main Loop
     print("Let's Play Hangman!")
     while not word_guessed and tries > 0:
@@ -97,21 +99,20 @@ def play(word):
                 for letter in word:
                     if letter in used_letters:
                         current_guess = current_guess + letter + " "
-                    else:    
+                    else:
                         current_guess = current_guess + "_ "
 
         else:
             print("Invalid Input")
-            continue
-
+        continue
     if tries == 0:
         print(display_hangman(tries))
         print("You are out of tries")
-        print("The word was", word,"!\n")
+        print("The word was", word, "!\n")
         play_again()
     else:
         print("Congratulations!\nYou "
-        "guessed the word", word,"! You Win!")
+              "guessed the word", word, "! You Win!")
         play_again()
 
 
@@ -120,12 +121,13 @@ def play_again():
     Ask user if they like to play the game again
     """
     play_again_option = input("Would you like to play the game again? \n"
-    "Type 1 for Yes or 2 for No:\n")
+                              "Type 1 for Yes or 2 for No:\n")
 
     # Making sure the user input is valid
     while play_again_option != "1" and play_again_option != "2":
         play_again_option = input("Invalid Input!\n"
-        "Please press 1 to play again and if not please press 2: \n")
+                                  "Please press 1 to play again and"
+                                  " if not please press 2: \n")
 
     # Take user to the relevant section of the game
     if play_again_option == "1":
@@ -133,7 +135,8 @@ def play_again():
     else:
         print("Thanks for playing Hangman. I hope you enjoyed it, Goodbye\n")
         print("If you like to play again, refresh the page by clicking "
-        "'run program option' at the top." )
+              "'run program option' at the top.")
+
 
 def display_hangman(tries):
     stages = ['''
@@ -193,8 +196,7 @@ def display_hangman(tries):
     =========
     ''']
     return stages[tries]
-        
-
+    
 welcome_msg()
 
 
